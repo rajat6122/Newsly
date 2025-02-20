50d96521079a2dfc72627ce595f0cb0e296d57b2
rajat6122 <mahajanrajat049@gmail.com>
2025-02-21 02:29:26 +0530

    improve the code

--- app/src/main/java/com/app/newsapp/retrofitapi/NewsApiService.kt
+++ app/src/main/java/com/app/newsapp/retrofitapi/NewsApiService.kt
@@ -4,12 +4,14 @@ import com.app.newsapp.model.NewsResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
-import com.app.newsapp.BuildConfig
 
 interface NewsApiService {
+    // Search by user
     @GET("everything")
     fun searchNews(
         @Query("q") query: String,
-        @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY
+//       @Query("apiKey") apiKey: String = "4eb4fe91f29040ea823bf2be3e2e474e"
+        @Query("apiKey") apiKey: String = "98d2e916d87a46538801e1c14c943b9c"
+
     ): Call<NewsResponse>
 }
